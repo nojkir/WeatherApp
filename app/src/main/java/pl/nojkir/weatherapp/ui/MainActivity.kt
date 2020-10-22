8e01d3811044c1b1717d918633df7dabb0cf6cf4
nojkir <neukirch.patryk@gmail.com>
2020-10-22 23:02:47 +0200

    BottomNav

--- app/src/main/java/pl/nojkir/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/pl/nojkir/weatherapp/ui/MainActivity.kt
@@ -2,7 +2,11 @@ package pl.nojkir.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import im.delight.android.location.SimpleLocation
+import kotlinx.android.synthetic.main.activity_main.*
 import pl.nojkir.weatherapp.R
 import pl.nojkir.weatherapp.databinding.ActivityMainBinding
 
@@ -12,5 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        bottomNavigationView.setupWithNavController(NavHostFragment.findNavController())
+
     }
 }
