33c21164bbca1aec0e39a54e8cdf6c965265f9c1
nojkir <neukirch.patryk@gmail.com>
2020-11-22 23:18:37 +0100

    Splash Screen

--- app/src/main/java/pl/nojkir/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/pl/nojkir/weatherapp/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import pl.nojkir.weatherapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
